c20e29893856e0bc6a59d782bf252e4169ff861e
Lauti0122 <93667112+Lauti0122@users.noreply.github.com>
2022-07-19 13:21:34 -0300

    initial commit

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,16 +1,17 @@
 var express = require('express');
 const router = express.Router();
 
+const foundations = require("./foundations");
+const news = require("./news");
+const pets = require("./pets");
+const users = require("./users");
 
 module.exports = router;
 
-router.use(express.json())
+router.use(express.json());
 
 router.use('/foundations', foundations); 
 router.use('/news', news);
 router.use('/pets', pets);
 router.use('/users', users);
-
-
-
 
