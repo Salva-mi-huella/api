e53e8b4371468541c5b65a95ddc5a145c7806136
CRISTIAN0026 <92878127+CRISTIAN0026@users.noreply.github.com>
2022-07-19 08:18:35 -0500

    creacion del modelo/pet

--- api/src/models/Pet.js
+++ api/src/models/Pet.js
@@ -3,10 +3,30 @@ const { DataTypes } = require('sequelize');
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('user', {
+  sequelize.define('pet', {
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    description:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    foundation: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    age: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    sex: {
+      type: DataTypes.ENUM("Macho", "Hembra"),
+      allowNull: false
+    },
+    image: {
+      type: DataTypes.STRING,
+      allowNull:false
+    }
+  },[]);
 };
