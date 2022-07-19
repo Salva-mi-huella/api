863d685d8536907689ddcf4d1c3c1070a9973439
Lauti0122 <93667112+Lauti0122@users.noreply.github.com>
2022-07-19 16:17:16 -0300

    C:/Program Files/Git/models/Foundation

--- api/src/models/Foundation.js
+++ api/src/models/Foundation.js
@@ -4,9 +4,47 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('foundation', {
+    id:{
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+      allowNull: false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    location:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    telephone_number:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email:{
+      type: DataTypes.STRING,
+    },
+    instagram:{
+      type: DataTypes.STRING,
+      
+    },
+    website:{
+      type: DataTypes.STRING,
+     
+    },
+    img:{
+      type: DataTypes.ARRAY(DataTypes.STRING),
+    },
+    password:{
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    rol_admin:{
+      type: DataTypes.BOOLEAN,
+      defaultValue: true
+    }
+  },{
+    timestamps: false,
   });
 };
