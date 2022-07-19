2a139b797e12fc1b1d1be30677e23fd1698b7bc3
CRISTIAN0026 <92878127+CRISTIAN0026@users.noreply.github.com>
2022-07-19 13:53:46 -0500

    creacion model/pet

--- api/src/models/Pet.js
+++ api/src/models/Pet.js
@@ -4,9 +4,34 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pet', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-  });
+    img: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false
+    },
+    age: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    genre: {
+      type: DataTypes.ENUM("Macho", "Hembra"),
+      allowNull: false
+    },
+    post_date: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.DATEONLY
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  }, {timestamps: false});
 };
