3f063e7bb2c6d9e3e71f4b1af596ed20d0cb2595
CRISTIAN0026 <92878127+CRISTIAN0026@users.noreply.github.com>
2022-07-20 14:40:46 -0500

    delete/foundations

--- api/src/routes/foundations.js
+++ api/src/routes/foundations.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { postFoundation, getFoundations } = require("../controllers/foundations"); 
+const { postFoundation, getFoundations, deleteFoundation } = require("../controllers/foundations"); 
 
 router.post("/", postFoundation);
 router.get("/", getFoundations);
+router.delete("/:id", deleteFoundation);
 
 module.exports = router;
