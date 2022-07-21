779ddb1216c189fc00d4b61bb4b21d578e86f46c
Lichi96 <lichilanda@hotmail.com>
2022-07-21 13:55:50 -0300

    Cambiado genre por gender

--- api/src/models/Pet.js
+++ api/src/models/Pet.js
@@ -24,7 +24,7 @@ module.exports = (sequelize) => {
       type: DataTypes.STRING,
       allowNull: false
     },
-    genre: {
+    gender: {
       type: DataTypes.ENUM("Macho", "Hembra"),
       allowNull: false
     },
