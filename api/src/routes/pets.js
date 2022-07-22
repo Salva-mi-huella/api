96c23e2673683b77de97eccba3d2d99ca5f16485
Lichi96 <66653514+Lichi96@users.noreply.github.com>
2022-07-21 22:21:13 -0300

    Update pets.js

--- api/src/routes/pets.js
+++ api/src/routes/pets.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { postPet, getPets, getPet } = require('../controllers/pets');
+const { postPet, getPets, getPetByID } = require('../controllers/pets');
 
 router.post("/", postPet);
 router.get("/", getPets);
-router.get("/:id", getPet);
+router.get("/:id", getPetByID);
 
 module.exports = router;
