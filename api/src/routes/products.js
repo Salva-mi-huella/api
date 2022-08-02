c304b2b8ea14cefdecbdc5e3a1edc30fc65bf089
Lauti0122 <93667112+Lauti0122@users.noreply.github.com>
2022-08-02 17:45:53 -0300

    updateProducts

--- api/src/routes/products.js
+++ api/src/routes/products.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const router = express.Router();
-const { postProduct, getProducts, getProductByID} = require("../controllers/products"); 
+const { postProduct, getProducts, getProductByID, deleteProductByID,updateProductByID} = require("../controllers/products"); 
 
 router.post("/", postProduct);
 router.get("/", getProducts);
 router.get("/:id", getProductByID);
-
+router.delete("/:id", deleteProductByID);
+router.put("/:id", updateProductByID);
 module.exports = router;
