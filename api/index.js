ab52fb0905c3de49775d2d579b0dbd5f865967da
Lisandro Landaburu <66653514+LichiLandaburu@users.noreply.github.com>
2022-10-27 02:05:28 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -4,7 +4,7 @@ const { conn } = require('./src/db.js');
 
 // Syncing all the models at once.
 conn.sync({ force: false }).then(() => {
-  server.listen(process.env.PORT, () => {
+  server.listen(process.env.PORT, process.env.HOST, () => {
     console.log(`%s listening at ${process.env.PORT}`); // eslint-disable-line no-console
   });
 });
